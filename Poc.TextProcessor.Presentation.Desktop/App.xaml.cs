056862d1c7f337028666941fae6f61b0217f73a7
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-01-25 11:48:49 +0100

    Implement WPF Basic UI

--- Poc.TextProcessor.Presentation.Desktop/App.xaml.cs
+++ Poc.TextProcessor.Presentation.Desktop/App.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Poc.TextProcessor.Business.Logic;
+using Poc.TextProcessor.Business.Logic.Abstractions;
+using Poc.TextProcessor.ResourceAccess.Mappers;
+using Poc.TextProcessor.ResourceAccess.Repositories;
+using Poc.TextProcessor.ResourceAccess.Repositories.Abstractions;
+using Poc.TextProcessor.Services;
+using Poc.TextProcessor.Services.Abstractions;
+using System.Text;
 using System.Windows;
 
 namespace Poc.TextProcessor.Presentation.Desktop
@@ -9,6 +16,36 @@ namespace Poc.TextProcessor.Presentation.Desktop
     /// </summary>
     public partial class App : Application
     {
-    }
+        private IServiceProvider? _serviceProvider;
 
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var serviceCollection = new ServiceCollection();
+
+            ConfigureServices(serviceCollection);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            _serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            // Main Window
+            services.AddTransient(typeof(MainWindow));
+
+            //TextService
+            services.AddTransient<ITextService, TextService>();
+            services.AddTransient<ITextLogic, TextLogic>();
+            services.AddTransient<ITextRepository, TextRepository>();
+            services.AddTransient<ITextMapper, TextMapper>();
+
+            //TextSortService
+            services.AddTransient<ITextSortService, TextSortService>();
+            services.AddTransient<ITextSortLogic, TextSortLogic>();
+            services.AddTransient<ITextSortRepository, TextSortRepository>();
+            services.AddTransient<ITextSortMapper, TextSortMapper>();
+        }
+    }
 }
--- Poc.TextProcessor.Presentation.Desktop/MainWindow.xaml.cs
+++ Poc.TextProcessor.Presentation.Desktop/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
-﻿using System.Text;
+﻿using Poc.TextProcessor.CrossCutting.Enums;
+using Poc.TextProcessor.CrossCutting.Globalization;
+using Poc.TextProcessor.ResourceAccess.Contracts;
+using Poc.TextProcessor.Services.Abstractions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Poc.TextProcessor.Presentation.Desktop
 {
@@ -16,9 +11,67 @@ namespace Poc.TextProcessor.Presentation.Desktop
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly ITextService _textService;
+        private readonly ITextSortService _textSortService;
+
+        // TODO: As an alternative, we could use a Rest API call using the collaborator pattern, which would handle these calls.
+        // We opted for the service layer for practicality and to avoid a direct dependency on the API.
+
+        public MainWindow(ITextService textService, ITextSortService textSortService)
         {
+            _textService = textService;
+            _textSortService = textSortService;
+
             InitializeComponent();
+            FillSortOptions();
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsInputValid())
+                return;
+
+            var selectedOption = SortOptions.SelectedItem as Sort;
+            var selectedOptionEnum = (SortOption)Enum.Parse(typeof(SortOption), selectedOption.Option);
+            var sortedText = _textSortService.Sort(TextInput.Text, selectedOptionEnum);
+            MessageBox.Show(sortedText);
+        }
+
+        private void CountButton_Click(object sender, RoutedEventArgs e)
+        {
+            var statistics = _textService.GetStatistics(TextInput.Text);
+            var message = $"Hyphens: {statistics.Hypens}, Words: {statistics.Words}, Spaces: {statistics.Spaces}";
+            MessageBox.Show(message);
+        }
+
+        private void RandomTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            var text = _textService.GetRandom();
+            TextInput.Text = text.Content;
+        }
+
+        private void FillSortOptions()
+        {
+            var orderOptions = _textSortService.List();
+            SortOptions.ItemsSource = orderOptions.Items;
+            SortOptions.DisplayMemberPath = "Option";
+        }
+
+        private bool IsInputValid()
+        {
+            if (string.IsNullOrWhiteSpace(TextInput.Text))
+            {
+                MessageBox.Show(Messages.InputFieldEmpty);
+                return false;
+            }
+
+            if (!(SortOptions.SelectedItem is Sort))
+            {
+                MessageBox.Show(Messages.SortFieldEmpty);
+                return false;
+            }
+
+            return true;
         }
     }
 }
