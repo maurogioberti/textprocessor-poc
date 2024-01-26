21b0909a2d04b06089e49a1a9892b7d3a3ccad67
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-01-26 15:42:40 +0100

    Refactor DTOs to Records for Improved Efficiency and Immutability

--- Poc.TextProcessor.ResourceAccess.Contracts/Collections/SortCollection.cs
+++ Poc.TextProcessor.ResourceAccess.Contracts/Collections/SortCollection.cs
@@ -1,14 +1,4 @@
 ﻿namespace Poc.TextProcessor.ResourceAccess.Contracts.Collections
 {
-    public class SortCollection
-    {
-        public SortCollection(IEnumerable<Sort> items, int totalCount)
-        {
-            Items = items;
-            TotalCount = totalCount;
-        }
-
-        public IEnumerable<Sort> Items { get; private set; }
-        public int TotalCount { get; private set; }
-    }
+    public record SortCollection(IEnumerable<Sort> Items, int TotalCount);
 }
--- Poc.TextProcessor.ResourceAccess.Contracts/Statistics.cs
+++ Poc.TextProcessor.ResourceAccess.Contracts/Statistics.cs
@@ -1,9 +1,9 @@
 ﻿namespace Poc.TextProcessor.ResourceAccess.Contracts
 {
-    public class Statistics
+    public record Statistics
     {
-        public int Hypens { get; set; }
-        public int Words { get; set; }
-        public int Spaces { get; set; }
+        public int Hypens { get; init; }
+        public int Words { get; init; }
+        public int Spaces { get; init; }
     }
 }
